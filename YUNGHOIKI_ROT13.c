ed5b01ac64f6acb895883093b1c6c69ca3f28277
hoi88ki <14074153d@connect.polyu.hk>
2015-06-06 10:43:31 +0800

    Update YUNGHOIKI_ROT13.c

--- YUNGHOIKI_ROT13.c
+++ YUNGHOIKI_ROT13.c
@@ -1,16 +1,25 @@
+// Author: Ivan Yung Hoi Ki
+// Date Created: 6 June 2015
+// This program is used to perform ROT13 encryption
+// ROT13 encryption is an encryption method which encryption and decryption is using the same program
+
 #include <stdio.h>
 
 char c;
 
 int main(){
 	do{
+		// read char
 		c = getc(stdin);
+		// check whether the file ends
 		if(feof(stdin))
 			break;
+		// encrypt all alphabet character accordingly
 		if(((c >= 'A')&&(c <= 'M'))||((c>='a')&&(c<='m')))
 			c += 13;
 		else if (((c >= 'N')&&(c <= 'Z'))||((c>='n')&&(c<='z')))
 			c -= 13;
+		// print out the encrypted character
 		printf("%c",c);
 	}while(1);
 	return 0;
